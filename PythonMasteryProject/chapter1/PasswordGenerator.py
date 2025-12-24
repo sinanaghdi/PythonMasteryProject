20b8d70b0824836b62f45ec20f4b9e684c8a1e9e
Sina Nanaqdi <nanaqdi48@gmail.com>
2025-12-24 16:30:09 +0330

    class based password generator finish

--- PythonMasteryProject/chapter1/PasswordGenerator.py
+++ PythonMasteryProject/chapter1/PasswordGenerator.py
@@ -1,18 +1,52 @@
 # modules and global variables
-
+from abc import ABC,abstractmethod
+import string
+import random
 
 
 # create password generator abstract class
-
+class PasswordGeneratorAbstract(ABC):
+    @abstractmethod
+    def generate_password(self,length=8):
+        pass
+        
+    
 
 # create numeric password generators
-
+class NumericPasswordGenerator(PasswordGeneratorAbstract):
+    letters = string.digits
+    
+    def generate_password(self,length=8):
+        
+       
+        return "".join (str(random.choice(self.letters)) 
+                        for _ in  range(length))      
 
 # create letter password generators
-
+class LettersPasswordGenerator(PasswordGeneratorAbstract):
+    letters = string.ascii_letters
+    
+    def generate_password(self, length=8):
+        return "".join (str(random.choice(self.letters)) 
+            for _ in  range(length))   
 
 # create mixed password generator
+class MixedPasswordGenerator(PasswordGeneratorAbstract):
+    letters = string.ascii_letters + string.digits
+    def generate_password(self, length=8):
+        return "".join (str(random.choice(self.letters)) 
+            for _ in  range(length))   
 
 
 
 # run the application
+generator = MixedPasswordGenerator()
+
+print(generator.generate_password())
+
+
+
+
+
+
+
