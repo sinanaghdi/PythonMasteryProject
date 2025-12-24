558af14335da655fa5c99bf1ae9f5967f0b57b6a
Sina Nanaqdi <nanaqdi48@gmail.com>
2025-12-24 16:12:11 +0330

    define the oop parts

---
+++ PythonMasteryProject/chapter1/PasswordGenerator.py
@@ -0,0 +1,18 @@
+# modules and global variables
+
+
+
+# create password generator abstract class
+
+
+# create numeric password generators
+
+
+# create letter password generators
+
+
+# create mixed password generator
+
+
+
+# run the application
