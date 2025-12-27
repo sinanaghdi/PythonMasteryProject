337eae5b6b9f373e812c66f4fd41a0c694a30896
Sina Nanaqdi <nanaqdi48@gmail.com>
2025-12-27 21:08:52 +0330

    initial commit

---
+++ PythonMasteryProject/chapter1/smart_salary_calculator.py
@@ -0,0 +1,32 @@
+# global variables and modules
+"""
+Docstring for chapter1.smart_salary_calculator!
+Write a program for the accounting department of a startup company. This program should:
+
+Take as input: employee name, base salary (in Tomans), and number of overtime hours.
+
+Calculate:
+
+The rate for each overtime hour is 1.5 times the regular hourly rate (assuming 160 hours of work per month).
+
+If the total salary (base + overtime) was below 12 million Tomans, the tax is 0%.
+
+If it was between 12 and 20 million, the tax is 10% on the amount in excess of 12 million.
+
+If it was above 20 million, the tax is 20% on the amount in excess of 20 million (plus the previous tax bracket).
+
+Output: Pay slip including name, gross salary, tax amount, and net salary received.
+"""
+
+
+
+
+# employee input
+
+
+
+# calculate the taxes
+
+
+
+# run the code
