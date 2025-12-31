0def786f87a7f82ea36054cc89e02acaed021083
Sina Nanaqdi <nanaqdi48@gmail.com>
2025-12-31 13:53:58 +0330

    use regex to find number

--- PythonMasteryProject/automation/finding-pattern.py
+++ PythonMasteryProject/automation/finding-pattern.py
@@ -1,3 +1,16 @@
 """
 this is automation script-- to find patterns with regex
 """
+import re
+
+phoneNumRegex = re.compile(r'(\d\d\d)-(\d\d\d)-(\d\d\d)')
+mo = phoneNumRegex.search('My number is 918-734-7840.') # mo stands for match object
+# print('Phone number found : ' + mo.group())
+# print(mo.group(1))
+# print(mo.group(2))
+# print(mo.group(3))
+print(mo.groups())
+areaCode, mainNumber = mo.groups()
+print(areaCode)
+# # print(mainNumber)
+
